464c15de2ce0a6f499a48013e015fd1e4bc2007d
Hideo Yanagi <hideo.yanagi@gmail.com>
2012-10-11 10:48:25 +0900

    support for the latest version mongoid.

--- lib/mongoid/acts_as_list/list/root.rb
+++ lib/mongoid/acts_as_list/list/root.rb
@@ -11,10 +11,14 @@ module Mongoid::ActsAsList
         private
 
         def define_position_scope(scope_name)
-          raise List::ScopeMissingError, "#acts_as_list requires a scope option" if scope_name.blank?
+          #raise List::ScopeMissingError, "#acts_as_list requires a scope option" if scope_name.blank?
 
-          scope_name = "#{scope_name}_id".intern if scope_name.to_s !~ /_id$/
-          define_method(:scope_condition) { {scope_name => self[scope_name]} }
+          if scope_name.blank?
+            define_method(:scope_condition) { {_id: {"$ne" => nil}} }
+          else
+            scope_name = "#{scope_name}_id".intern if scope_name.to_s !~ /_id$/
+						define_method(:scope_condition) { {scope_name => self[scope_name]} }	
+ 	       	end
         end
       end
 
@@ -27,7 +31,8 @@ module Mongoid::ActsAsList
 
         criteria = criteria.to_criteria if criteria.is_a? self.class
 
-        db.collection(collection.name).update(criteria.selector, {"$inc" => { position_field => by_how_much }}, {multi: true})
+        #db.collection(collection.name).update(criteria.selector, {"$inc" => { position_field => by_how_much }}, {multi: true})
+      	self.class.where(criteria.selector).update({"$inc" => { position_field => by_how_much }})
       end
 
       def to_criteria
